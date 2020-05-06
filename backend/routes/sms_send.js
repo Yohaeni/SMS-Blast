c0be976df050ed48af9742dff89dabe489599670
choiyoh <choeyoh@gmail.com>
2020-05-06 17:58:59 +0800

    SMS Blast First Demo version

---
+++ backend/routes/sms_send.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/', function(req, res, next) {
+    var request = require("request");
+    var shortcode = '8380';
+    var access_token = 'rTT2IoOKm3uPXdaaS-WRD-WPOYN7ruPFhgEU33L62Bo';
+    var address = req.body.address;
+    var clientCorrelator = '123456';
+    var message = req.body.message;
+    var options = { method: 'POST',
+    url: 'https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/' + shortcode + '/requests',
+    qs: { 'access_token': access_token },
+    headers: 
+    { 'Content-Type': 'application/json' },
+    body: 
+    { 'outboundSMSMessageRequest': 
+        { 'clientCorrelator': clientCorrelator,
+        'senderAddress': shortcode,
+        'outboundSMSTextMessage': { 'message': message },
+        'address': address } },
+    json: true };
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        console.log(body);
+    });
+    res.send('SMS Page');
+});
+
+
+
+module.exports = router;
---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div>
+      <input placeholder="mobile number" v-model="address">
+    </div>
+    <div>
+      <textarea placeholder="Enter the message" v-model="message"></textarea>
+    </div>
+    <div>
+       <button @click="sendMessage">Send Message</button>
+    </div> 
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+import axios from "axios";
+
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      address: "",
+      message: ""
+    }
+  },
+
+  methods: {
+    sendMessage() {
+      this.$http.post('/api', {
+        address: this.address, message: this.message
+      })
+      .then((res) => {
+        console.log(res)
+      })
+    }
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>

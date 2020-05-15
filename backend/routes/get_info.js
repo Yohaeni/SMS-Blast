143406d466b35953c6fe6002c36d36e585ff63a2
choiyoh <choeyoh@gmail.com>
2020-05-15 18:17:12 +0800

    error fixing

--- backend/routes/get_info.js
+++ backend/routes/get_info.js
@@ -21,7 +21,9 @@ router.post('/', function(req, res, next) {
     var appId = 'zodGIE6Rb8tBginKBEcR6gtEaoozIyoy';
     var appSecret = 'bbdfb979db0094746acf9008a70095fafccfed0401eabf5fce5fa173ce2d7fc6';
     var code = req.body.code;
-    var options = { method: 'POST',
+    var access_token = "";
+    var subscriber_number = "";
+    var options = { method: 'POST',   
     url: 'https://developer.globelabs.com.ph/oauth/access_token?app_id=' + appId + '&app_secret=' + appSecret + '&code=' + code,
     };
 
@@ -29,25 +31,23 @@ router.post('/', function(req, res, next) {
         if (error) throw new Error(error);
 
         var bodyString = response.body.split('"');
-        var access_token = bodyString[3];
-        var subscriber_number = bodyString[7];
-
-        connection.query("INSERT INTO SMS_Client (accesstoken, mobilenumber, subscriber_id) VALUES ('" + access_token + "', '" + subscriber_number +" 1')",
-        function (error, result, fileds) {
-            if (error) {
-                res.send('err : ' + error)
-            }
-            else {
-                console.log(body)
-                res.send('success' + body)
-            }
-        })
+        access_token = bodyString[3];
+        subscriber_number = bodyString[7];
         
-        console.log("Access_Token : " + access_token);
-        console.log("Mobile number : " + subscriber_number);
         res.send(response);
     });
-    
+    console.log("Access_Token : " + access_token);
+        console.log("Mobile number : " + subscriber_number);
+    // connection.query("INSERT INTO SMS_Client (accesstoken, mobilenumber, subscriber_id) VALUES ('" + access_token + "', '" + subscriber_number +" 1')",
+    //     function (error, result, fileds) {
+    //         if (error) {
+    //             res.send('err : ' + error)
+    //         }
+    //         else {
+    //             console.log(body)
+    //             res.send('success' + body)
+    //         }
+    // });
 });
 
 

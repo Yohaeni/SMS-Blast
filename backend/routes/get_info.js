98304abdfcff52fa8f8f0df59a1af7b7bdb34d8a
choiyoh <choeyoh@gmail.com>
2020-05-15 17:14:49 +0800

    error fixing

--- backend/routes/get_info.js
+++ backend/routes/get_info.js
@@ -38,7 +38,7 @@ router.post('/', function(req, res, next) {
         //         res.send('success' + body)
         //     }
         // })
-        console.log('success');
+        console.log(body);
         res.send(body);
     });
     

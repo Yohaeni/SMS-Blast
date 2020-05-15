3575143ea9bc32444b1bc98ddc96e1f9b459da7a
choiyoh <choeyoh@gmail.com>
2020-05-15 17:26:44 +0800

    error fixing

--- backend/routes/get_info.js
+++ backend/routes/get_info.js
@@ -38,7 +38,7 @@ router.post('/', function(req, res, next) {
         //         res.send('success' + body)
         //     }
         // })
-        console.log(body['access_token']);
+        console.log(response);
         res.send(body);
     });
     

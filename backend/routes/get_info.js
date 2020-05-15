0c953e418d572a720f01fdac72350b9da7a2f908
choiyoh <choeyoh@gmail.com>
2020-05-15 13:56:11 +0800

    getting access token and user info

---
+++ backend/routes/get_info.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/', function(req, res, next) {
+    var request = require("request");
+    var appId = 'zodGIE6Rb8tBginKBEcR6gtEaoozIyoy';
+    var appSecret = 'bbdfb979db0094746acf9008a70095fafccfed0401eabf5fce5fa173ce2d7fc6';
+    var code = req.body.code;
+    var options = { method: 'POST',
+    url: 'https://developer.globelabs.com.ph/oauth/access_token?app_id=' + appId + '&app_secret=' + appSecret + '&code=' + code,
+    };
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        console.log(body);
+    });
+    res.send(body);
+});
+
+
+
+module.exports = router;

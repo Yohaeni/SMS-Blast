286e0ad5dcad5ff10a07f8ae9f32dc3ba36b3de5
choiyoh <choeyoh@gmail.com>
2020-05-15 18:11:30 +0800

    error fixing

--- backend/routes/get_info.js
+++ backend/routes/get_info.js
@@ -32,7 +32,7 @@ router.post('/', function(req, res, next) {
         var access_token = bodyString[3];
         var subscriber_number = bodyString[7];
 
-        connection.query("INSERT INTO SMS_Client (companyid, accesstoken, mobilenumber, subscriber_id) VALUES ('1," + access_token + "', '" + subscriber_number +" 1')",
+        connection.query("INSERT INTO SMS_Client (accesstoken, mobilenumber, subscriber_id) VALUES ('" + access_token + "', '" + subscriber_number +" 1')",
         function (error, result, fileds) {
             if (error) {
                 res.send('err : ' + error)

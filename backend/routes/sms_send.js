8532dca3b6818edf6361fd16356301a23c885f52
choiyoh <choeyoh@gmail.com>
2020-05-20 17:30:53 +0800

    add multi number sending function

--- backend/routes/sms_send.js
+++ backend/routes/sms_send.js
@@ -10,109 +10,86 @@ router.post('/', function (req, res, next) {
     var clientCorrelator = '123456';
     var message = req.body.message;
 
-    if (address.length > 10) {
+    if (address.length > 11) {
+        addresses = address.split(',');
+    }
+    // If there is only one mobile number
+    if (addresses.length < 1) {
         // If there is only one number with 11 digits.
         if (address.length == 11) {
             address = address.substr(1, 10);
-        } else if (address.length > 11) { // If there are more than 1 number
-            addresses = address.split(',');
         }
     }
 
-    // var options = {
-    //     method: 'POST',
-    //     url: 'https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/' + shortcode + '/requests',
-    //     qs: {
-    //         'access_token': access_token
-    //     },
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     },
-    //     body: {
-    //         'outboundSMSMessageRequest': {
-    //             'clientCorrelator': clientCorrelator,
-    //             'senderAddress': shortcode,
-    //             'outboundSMSTextMessage': {
-    //                 'message': message
-    //             },
-    //             'address': address
-    //         }
-    //     },
-    //     json: true
-    // };
-
-    // request(options, function (error, response, body) {
-    //     if (error) throw new Error(error);
-
-    //     console.log(body);
-    //     res.send(body);
-    // });
-
-    // If there is only one mobile number
-    if (addresses.length < 1) {
-        var options = {
-            method: 'POST',
-            url: 'https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/' + shortcode + '/requests',
-            qs: {
-                'access_token': access_token
-            },
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: {
-                'outboundSMSMessageRequest': {
-                    'clientCorrelator': clientCorrelator,
-                    'senderAddress': shortcode,
-                    'outboundSMSTextMessage': {
-                        'message': message
-                    },
-                    'address': address
-                }
-            },
-            json: true
-        };
-
-        request(options, function (error, response, body) {
-            if (error) throw new Error(error);
-
-            console.log(body);
-            res.send(body);
-        });
-    }
-    // If there are more than one number
-    else {
-        for (var i = 0; i < addresses.length; i++) {
-            var number = addresses[i];
-            var options = {
-                method: 'POST',
-                url: 'https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/' + shortcode + '/requests',
-                qs: {
-                    'access_token': access_token
+    var options = {
+        method: 'POST',
+        url: 'https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/' + shortcode + '/requests',
+        qs: {
+            'access_token': access_token
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: {
+            'outboundSMSMessageRequest': {
+                'clientCorrelator': clientCorrelator,
+                'senderAddress': shortcode,
+                'outboundSMSTextMessage': {
+                    'message': message
                 },
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: {
-                    'outboundSMSMessageRequest': {
-                        'clientCorrelator': clientCorrelator,
-                        'senderAddress': shortcode,
-                        'outboundSMSTextMessage': {
-                            'message': message
-                        },
-                        'address': number
-                    }
-                },
-                json: true
-            };
+                'address': address
+            }
+        },
+        json: true
+    };
 
-            request(options, function (error, response, body) {
-                if (error) throw new Error(error);
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
 
-                console.log(body);
-                res.send(body);
-            });
+        console.log(body);
+        res.send(body);
+    });
+}
+// If there are more than one number
+else {
+    for (var i = 0; i < addresses.length; i++) {
+        var number = addresses[i];
+        // If there is only one number with 11 digits.
+        if (number.length == 11) {
+            number = number.substr(1, 10);
         }
     }
+
+    var options = {
+        method: 'POST',
+        url: 'https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/' + shortcode + '/requests',
+        qs: {
+            'access_token': access_token
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: {
+            'outboundSMSMessageRequest': {
+                'clientCorrelator': clientCorrelator,
+                'senderAddress': shortcode,
+                'outboundSMSTextMessage': {
+                    'message': message
+                },
+                'address': number
+            }
+        },
+        json: true
+    };
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        console.log(body);
+        res.send(body);
+    });
+}
+}
 
 });
 

c5dce30200fc9f1a5117abf202ec3d4675b6fdbc
choiyoh <choeyoh@gmail.com>
2020-05-20 17:33:17 +0800

    add multi number sending function

--- backend/routes/sms_send.js
+++ backend/routes/sms_send.js
@@ -19,78 +19,76 @@ router.post('/', function (req, res, next) {
         if (address.length == 11) {
             address = address.substr(1, 10);
         }
+
+        var options = {
+            method: 'POST',
+            url: 'https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/' + shortcode + '/requests',
+            qs: {
+                'access_token': access_token
+            },
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: {
+                'outboundSMSMessageRequest': {
+                    'clientCorrelator': clientCorrelator,
+                    'senderAddress': shortcode,
+                    'outboundSMSTextMessage': {
+                        'message': message
+                    },
+                    'address': address
+                }
+            },
+            json: true
+        };
+
+        request(options, function (error, response, body) {
+            if (error) throw new Error(error);
+
+            console.log(body);
+            res.send(body);
+        });
     }
-
-    var options = {
-        method: 'POST',
-        url: 'https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/' + shortcode + '/requests',
-        qs: {
-            'access_token': access_token
-        },
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: {
-            'outboundSMSMessageRequest': {
-                'clientCorrelator': clientCorrelator,
-                'senderAddress': shortcode,
-                'outboundSMSTextMessage': {
-                    'message': message
-                },
-                'address': address
+    // If there are more than one number
+    else {
+        for (var i = 0; i < addresses.length; i++) {
+            var number = addresses[i];
+            // If there is number with 11 digits.
+            if (number.length == 11) {
+                number = number.substr(1, 10);
             }
-        },
-        json: true
-    };
 
-    request(options, function (error, response, body) {
-        if (error) throw new Error(error);
 
-        console.log(body);
-        res.send(body);
-    });
-}
-// If there are more than one number
-else {
-    for (var i = 0; i < addresses.length; i++) {
-        var number = addresses[i];
-        // If there is only one number with 11 digits.
-        if (number.length == 11) {
-            number = number.substr(1, 10);
+            var options = {
+                method: 'POST',
+                url: 'https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/' + shortcode + '/requests',
+                qs: {
+                    'access_token': access_token
+                },
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: {
+                    'outboundSMSMessageRequest': {
+                        'clientCorrelator': clientCorrelator,
+                        'senderAddress': shortcode,
+                        'outboundSMSTextMessage': {
+                            'message': message
+                        },
+                        'address': number
+                    }
+                },
+                json: true
+            };
+
+            request(options, function (error, response, body) {
+                if (error) throw new Error(error);
+
+                console.log(body);
+                res.send(body);
+            });
         }
     }
-
-    var options = {
-        method: 'POST',
-        url: 'https://devapi.globelabs.com.ph/smsmessaging/v1/outbound/' + shortcode + '/requests',
-        qs: {
-            'access_token': access_token
-        },
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: {
-            'outboundSMSMessageRequest': {
-                'clientCorrelator': clientCorrelator,
-                'senderAddress': shortcode,
-                'outboundSMSTextMessage': {
-                    'message': message
-                },
-                'address': number
-            }
-        },
-        json: true
-    };
-
-    request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-
-        console.log(body);
-        res.send(body);
-    });
-}
-}
-
 });
 
 

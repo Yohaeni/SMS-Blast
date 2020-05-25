b33e643509b396660a1cc3f91b080fb12d5291d2
choiyoh <choeyoh@gmail.com>
2020-05-25 15:59:21 +0800

    Time checker added

--- backend/routes/sms_send.js
+++ backend/routes/sms_send.js
@@ -187,8 +187,8 @@ router.post("/", function (req, res, next) {
                     //         console.log(body);
                     //     });
                     var times = new Date();
-                    var timestamp = times.getFullYear() + "-" + (times.getMonth() + 1) + "-" + times.getDate() + " " + times.getHours() + ":" + times.getMinutes() + ":"
-                    times.getSeconds() + "." + times.getMilliseconds();
+                    var timestamp = times.getFullYear() + "-" + (times.getMonth() + 1) + "-" + times.getDate() + " " + times.getHours() + ":" + times.getMinutes() + ":" +
+                        times.getSeconds() + "." + times.getMilliseconds();
                     var smsQuery = "INSERT INTO sms_message (recipients,message,timestamp) VALUES ('" + fullName + "', '" + sendingMessage + "', '" + timestamp + "')";
 
                     connection.query(smsQuery, function (err, response) {

f00dc689d1ef85c135bc4f7db97c5dfaae3c724c
choiyoh <choeyoh@gmail.com>
2020-05-25 16:03:17 +0800

    Time checker added

--- backend/routes/sms_send.js
+++ backend/routes/sms_send.js
@@ -180,21 +180,25 @@ router.post("/", function (req, res, next) {
                         json: true,
                     };
 
-                    // request(options,
-                    //     function (error, response, body) {
-                    //         if (error) throw new Error(error);
+                    request(options,
+                        function (error, response, body) {
+                            if (error) {
+                                throw new Error(error);
+                            } else {
+                                var times = new Date();
+                                var timestamp = times.getFullYear() + "-" + (times.getMonth() + 1) + "-" + times.getDate() + " " + times.getHours() + ":" + times.getMinutes() + ":" +
+                                    times.getSeconds() + "." + times.getMilliseconds();
+                                var smsQuery = "INSERT INTO sms_message (recipients,message,timestamp) VALUES ('" + fullName + "', '" + sendingMessage + "', '" + timestamp + "')";
 
-                    //         console.log(body);
-                    //     });
-                    var times = new Date();
-                    var timestamp = times.getFullYear() + "-" + (times.getMonth() + 1) + "-" + times.getDate() + " " + times.getHours() + ":" + times.getMinutes() + ":" +
-                        times.getSeconds() + "." + times.getMilliseconds();
-                    var smsQuery = "INSERT INTO sms_message (recipients,message,timestamp) VALUES ('" + fullName + "', '" + sendingMessage + "', '" + timestamp + "')";
+                                connection.query(smsQuery, function (err, response) {
+                                    if (err) throw err;
+                                    console.log(response);
+                                })
+                            }
 
-                    connection.query(smsQuery, function (err, response) {
-                        if (err) throw err;
-                        console.log(response);
-                    })
+                            console.log(body);
+                        });
+
 
                     i++;
                 }

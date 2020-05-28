ab144a95454b1cbc75aaa08ab979ed2b5d92918f
choiyoh <choeyoh@gmail.com>
2020-05-28 12:42:07 +0800

    SSL certification added

--- backend/routes/get_history.js
+++ backend/routes/get_history.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res, next) {
+router.post('/', function (req, res, next) {
     var mysql = require('mysql');
 
     //Create mysql connection

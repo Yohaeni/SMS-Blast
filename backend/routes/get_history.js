37a5c880ea596928456cb9249ee96327d5b6e226
choiyoh <choeyoh@gmail.com>
2020-05-28 14:47:53 +0800

    CORS Fixing

--- backend/routes/get_history.js
+++ backend/routes/get_history.js
@@ -20,7 +20,7 @@ router.post('/', function (req, res, next) {
         console.log("Connected!");
     });
 
-    connection.query("SELECT recipients,message,timestamp FROM `sms_message` ORDER BY timestamp DESC LIMIT 10", function (err, result) {
+    connection.query("SELECT recipients,message,timestamp FROM `sms_message` ORDER BY timestamp DESC LIMIT 30", function (err, result) {
         res.json(result);
     });
 });
